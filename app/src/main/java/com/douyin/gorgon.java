455835954951c1e3bdaf26f011b812800a8b0f17
hello245m <72144633+hello245m@users.noreply.github.com>
2022-04-23 18:48:02 +0800

    Update gorgon.java

--- app/src/main/java/com/douyin/gorgon.java
+++ app/src/main/java/com/douyin/gorgon.java
@@ -59,13 +59,7 @@ public class gorgon {
         byte[] leviathan = com.ss.sys.ces.a.leviathan(-1, ks, com.ss.a.b.a.ac(URL + X_SS_STUB + COOKIE + SESSION_ID));
         return com.ss.a.b.a.ab(leviathan);
     }
-    public static String getGorgonBystr(String shijian,String pingjie) {
-        int ks = Integer.parseInt(shijian);
-        byte[] leviathan = com.ss.sys.ces.a.leviathan(-1, ks, com.ss.a.b.a.ac(pingjie));
-        return com.ss.a.b.a.ab(leviathan);
-    }
-
-    public static String getGorgon(String url, HashMap<String, String> headers) {
+        public static String getGorgon(String url, HashMap<String, String> headers) {
         String urlParams = getUrlParams(url);
         String URL = encryptData(urlParams);
         long rticket = System.currentTimeMillis();
